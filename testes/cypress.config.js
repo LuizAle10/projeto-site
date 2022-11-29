22b2d8a3333387c012807e24919254e4f2932e53
LuizAle10 <alebispodossantos@gmail.com>
2022-11-28 23:35:53 -0300

    ajuste arquivo report

--- testes/cypress.config.js
+++ testes/cypress.config.js
@@ -7,15 +7,8 @@ module.exports = defineConfig({
   reporter: 'junit',
     reporterOptions: {
       mochaFile: 'results/TEST-[hash].xml'
-    },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-  
-    },
-  },
-});
+    }
+  })
 
 
 require('@applitools/eyes-cypress')(module);
