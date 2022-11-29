707799220f117308d36cd488b809acd92f380c8d
LuizAle10 <alebispodossantos@gmail.com>
2022-11-29 00:00:01 -0300

    Revert "ajuste arquivo report"
    This reverts commit 22b2d8a3333387c012807e24919254e4f2932e53.

--- testes/cypress.config.js
+++ testes/cypress.config.js
@@ -7,8 +7,15 @@ module.exports = defineConfig({
   reporter: 'junit',
     reporterOptions: {
       mochaFile: 'results/TEST-[hash].xml'
-    }
-  })
+    },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+  
+    },
+  },
+});
 
 
 require('@applitools/eyes-cypress')(module);
